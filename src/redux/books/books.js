2952ff0bdfaf9f928b2dfbf4cbc5ad3265c9806d
Vanessa O <vaneolipa@gmail.com>
2022-12-14 21:35:35 -0500

    Add book's actions and reducer

---
+++ src/redux/books/books.js
@@ -0,0 +1,29 @@
+const ADD_BOOK = 'books/ADD_BOOK';
+const REMOVE_BOOK = 'books/REMOVE_BOOK';
+
+const initialState = [];
+
+export const addBook = (book) => ({
+  type: ADD_BOOK,
+  book,
+});
+
+export const removeBook = (bookId) => ({
+  type: REMOVE_BOOK,
+  bookId,
+});
+
+const booksReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case ADD_BOOK:
+      return [...state, action.book];
+    case REMOVE_BOOK: {
+      const filterBooks = state.filter((book) => book.id !== action.bookId);
+      return filterBooks;
+    }
+    default:
+      return state;
+  }
+};
+
+export default booksReducer;
