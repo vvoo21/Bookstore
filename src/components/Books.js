74830b09f994046da8a7766b38caea57078c6cb6
Vanessa O <vaneolipa@gmail.com>
2022-12-15 20:54:33 -0500

    add a functionality for "Remove" button

--- src/components/Books.js
+++ src/components/Books.js
@@ -1,15 +1,22 @@
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { removeBook } from '../redux/books/books';
 import Book from './Book';
 import Form from './Form';
 
 const Books = () => {
+  const dispatch = useDispatch();
   const booksList = useSelector((state) => state.books);
   return (
     <>
       <div className="book-list-container">
         <ul className="book-list">
           {booksList.map((book) => (
-            <Book key={book.id} genre={book.genre} title={book.title} author={book.author} />
+            <li key={book.id}>
+              <Book genre={book.genre} title={book.title} author={book.author} />
+              <div>
+                <button type="button" onClick={() => dispatch(removeBook(book.id))}>Remove</button>
+              </div>
+            </li>
           ))}
         </ul>
       </div>
