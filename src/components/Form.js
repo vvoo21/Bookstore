97ed716728c6b55ddbf2ecc558c97a165064c734
Vanessa O <vaneolipa@gmail.com>
2022-12-17 16:29:05 -0500

    Add Books style

--- src/components/Form.js
+++ src/components/Form.js
@@ -34,29 +34,30 @@ const Form = () => {
   };
 
   return (
-    <div>
+    <section className="form-container">
       <h2>ADD NEW BOOK</h2>
       <form>
-        <input id="title" type="text" name="title" value={newBook.title} onInput={handleInput} placeholder="Book title" />
-        <input id="author" type="text" name="author" value={newBook.author} onInput={handleInput} placeholder="Author" />
-        <select name="category" id="category" value={newBook.category} onInput={handleInput} placeholder="category">
+        <input id="title" type="text" name="title" value={newBook.title} onInput={handleInput} placeholder="Book title" aria-label="title" required />
+        <input id="author" type="text" name="author" value={newBook.author} onInput={handleInput} placeholder="Author" aria-label="author" required />
+        <select name="category" id="category" value={newBook.category} onInput={handleInput} placeholder="category" aria-label="category" required>
           <option value="" disabled>Category</option>
-          <option value="fantasy">Fantasy</option>
-          <option value="science fiction">Science Fiction</option>
-          <option value="action">Action</option>
-          <option value="mystery">Mystery</option>
-          <option value="thriller">Thriller</option>
-          <option value="romance">Romance</option>
-          <option value="magical realism">Magical Realism</option>
-          <option value="children">Children’s</option>
-          <option value="biography">Biography</option>
-          <option value="food & drink">Food & Drink</option>
-          <option value="self-help">Self-help</option>
-          <option value="economy">Economy</option>
+          <option value="Fantasy">Fantasy</option>
+          <option value="Science Fiction">Science Fiction</option>
+          <option value="Action">Action</option>
+          <option value="Mystery">Mystery</option>
+          <option value="Thriller">Thriller</option>
+          <option value="Romance">Romance</option>
+          <option value="Magical Realism">Magical Realism</option>
+          <option value="Children">Children’s</option>
+          <option value="Biography">Biography</option>
+          <option value="Food & Drink">Food & Drink</option>
+          <option value="Self-Help">Self-help</option>
+          <option value="Economy">Economy</option>
+          <option value="History">History</option>
         </select>
         <button type="submit" onClick={handleClick}>ADD BOOK</button>
       </form>
-    </div>
+    </section>
   );
 };
 
